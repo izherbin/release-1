2c866806870da8ad69cf42131527e5343fdbe1cd
33and2 <sevpor26@gmail.com>
2021-03-19 13:06:16 -0500

    fix: typo

--- src/templates/Template.js
+++ src/templates/Template.js
@@ -1,16 +1,12 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles() => ({
-  container: {
-  }
-});
+const useStyles = makeStyles(() => ({
+  container: {},
+}));
 
 export const Template = () => {
   const { container } = useStyles();
 
-  return (
-    <div className={container}>
-    </div>
-  );
+  return <div className={container} />;
 };
