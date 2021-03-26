c3f1ba2279a7e6bcad6009eff9c7daa7d0526ab9
33and2 <sevpor26@gmail.com>
2021-03-26 10:17:29 -0500

    fix: endpoint url

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,9 +4,12 @@ import { SearchTable } from 'components/layout/SearchTable/SearchTable';
 import { Pagination } from 'components/UI/Pagination';
 import { useMedia } from 'hooks/useMedia';
 import { SearchTableMobile } from 'components/layout/SearchTable/SearchTableMobile';
+import { useFetchData } from 'hooks/useFetchData';
 
 const Index = () => {
   const { matchesMobile } = useMedia();
+  const { isData, isLoading } = useFetchData();
+  console.log('Index -> isData', isData);
 
   const search = (props) =>
     matchesMobile
