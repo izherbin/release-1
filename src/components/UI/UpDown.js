2c4c12d8832db9c18c71f6123fa3b8b41d67394e
33and2 <sevpor26@gmail.com>
2021-03-31 13:32:42 -0500

    fix:

--- src/components/UI/UpDown.js
+++ src/components/UI/UpDown.js
@@ -7,22 +7,21 @@ const useStyles = makeStyles(({ palette: { secondary, blue } }) => ({
   },
   top: {
     transform: 'translate(0, 1px)',
-    fill: ({ isOrdered, index, isUp, ids }) =>
-      isOrdered === ids[index - 1] && isUp ? blue : secondary.main,
+    fill: ({ isOrdered, index, isUp }) =>
+      isOrdered === `order${index}` && isUp ? blue : secondary.main,
   },
   bottom: {
     transform: 'rotate(180deg) translate(8px, -6px)',
-    fill: ({ isOrdered, index, isUp, ids }) =>
-      isOrdered === ids[index - 1] && !isUp ? blue : secondary.main,
+    fill: ({ isOrdered, index, isUp }) =>
+      isOrdered === `order${index}` && !isUp ? blue : secondary.main,
   },
 }));
 
-export const UpDown = ({ index = 0, isOrdered, orderHandler, isUp }) => {
+export const UpDown = ({ index, isOrdered, orderHandler, isUp }) => {
   const { container, top, bottom } = useStyles({ isOrdered, index, isUp });
-  const ids = ['niche', 'volume', 'growth', 'trend'];
 
   return (
-    <div className={container} onClick={orderHandler} id={ids[index - 1]}>
+    <div className={container} onClick={orderHandler} id={`order${index}`}>
       <svg height="4" viewBox="0 0 8 4" width="8" className={top}>
         <path
           d="m3.53553391 2.03553391h5l-5 5z"
